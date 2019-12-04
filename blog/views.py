1b910eece6923daf400bc637bfd8e3e8440ec55a
Jack Feng <hubster.coder@gmail.com>
2019-12-03 21:45:10 -0500

    create users app and register form

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'RedJay',
-        'title': 'Blog Post 1',
-        'content': 'first post content',
-        'date_posted': 'December 1, 2019'
-    },
-    {
-        'author': 'BlueJay',
-        'title': 'Blog Post 2',
-        'content': 'second post content',
-        'date_posted': 'December 2, 2019'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
